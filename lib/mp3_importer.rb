28a7f5019a8831b39638b16563abad56ee0e4a22
Elliott Vargo <vargoelliott@gmail.com>
2021-04-07 01:37:02 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,9 +10,9 @@ def files
   @files.delete_if {|f| f == ".." || f == "."}
 end
 
-def import(file_name)
+def import
 self.files.each do |file|
-  song = Song.new_by_filename(file_name)
+  song = Song.new_by_filename(file)
   Artist.all << song.artist unless Artist.all.include?(song.artist)
 end
 end
