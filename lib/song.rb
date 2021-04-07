adbcf940b38a754b546aaf3a035feda7693bc64a
Elliott Vargo <vargoelliott@gmail.com>
2021-04-07 01:05:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ def self.all
 end
 
 def self.new_by_filename(file_name)
-new_Song = Song.new(file_name.split(" - ")[1])
+new_song = Song.new(file_name.split(" - ")[1])
 new_song.artist = Song.new(file_name.split(" - ")[0])
 new_song
 
